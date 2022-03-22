deb8c5c949f5fffb380fc1343971c015f49198b5
chadat23 <chad.derosier@tutanota.com>
2022-03-21 17:04:33 -0700

    Setting basic processing stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use gui_rs::guiposition::guisizes::{GUISize, SetSize};
+use gui_rs::guiprocessing::GUIProcessing;
 use gui_rs::guiwindow::GUIWindow;
 
 fn main() {
     // let window = GUIWindow::new().set_width(400).set_height(600).set_title(String::from("Hello"));
     let mut window = GUIWindow::default();
     window
-        .set_width(GUISize::from_pixels(650.))
+        .set_width(GUISize::from_pixels(850.))
         .set_height(GUISize::from_pixels(550.))
         .set_title(String::from("Hello"));
-    window.start();
+
+    let processing = GUIProcessing::default();
+    
+    window.start(processing);
 }
