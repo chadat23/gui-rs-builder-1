f3830ad223daf37d7054cb3ec54bec3cf7e5624a
Chad DeRosier <chad.derosier@tutanota.com>
2022-03-21 21:31:28 -0700

    Updated for guisize to guilength stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use gui_rs::guiposition::guisizes::{GUISize, SetSize};
+use gui_rs::guiposition::guilengths::{GUILength, SetLength};
 use gui_rs::guiprocessing::GUIProcessing;
 use gui_rs::guiwindow::GUIWindow;
 
@@ -6,8 +6,8 @@ fn main() {
     // let window = GUIWindow::new().set_width(400).set_height(600).set_title(String::from("Hello"));
     let mut window = GUIWindow::default();
     window
-        .set_width(GUISize::from_pixels(850.))
-        .set_height(GUISize::from_pixels(550.))
+        .set_width(GUILength::from_pixels(850.))
+        .set_height(GUILength::from_pixels(550.))
         .set_title(String::from("Hello"));
 
     let processing = GUIProcessing::default();
