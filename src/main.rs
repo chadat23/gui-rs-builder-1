675d5885b93c1ddee9f000b2a4d02697832bbfae
chadat23 <chad.derosier@tutanota.com>
2022-03-28 16:20:20 -0700

    Children of children

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use gui_rs::guiprocessing;
-use gui_rs::guiproperties::guicolor::GUIColor;
 use gui_rs::guiproperties::guiposition::GUISize;
+use gui_rs::guiproperties::guitraits::*;
+use gui_rs::guiproperties::GUIColor;
 use gui_rs::guiresources::GUIResources;
-use gui_rs::guiwidgets::{GUIWindow, GUIButton};
+use gui_rs::guiwidgets::{GUIButton, GUIWindow};
 
 fn main() {
     // let window = GUIWindow::new().set_width(400).set_height(600).set_title(String::from("Hello"));
     let mut window = GUIWindow::default();
-    window
-        .set_size(GUISize::from_pixels(800, 300))
-        .set_title("Hello")
-        .set_background_color(GUIColor::from_rgba_u8u8u8u8(50, 50, 50, 255));
+    window.set_size(GUISize::from_pixels(800, 300));
+    window.set_title("Hello");
+    window.set_background_color(GUIColor::from_rgba_u8u8u8u8(50, 50, 50, 255));
 
     let button = GUIButton::default();
-    window.add_child(Box::from(button));
+    window.add_area_child(Box::from(button));
 
     let resources = GUIResources::default();
 
