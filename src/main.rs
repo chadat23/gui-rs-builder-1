f0d6b988a6bbeffa98fba17faa9fa440dc2dc7b2
Chad DeRosier <chad.derosier@tutanota.com>
2022-03-31 00:11:55 -0700

    Accounting for various length types

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use gui_rs::guiwidgets::{GUIButton, GUIWindow};
 fn main() {
     // let window = GUIWindow::new().set_width(400).set_height(600).set_title(String::from("Hello"));
     let mut window = GUIWindow::default();
-    window.set_size(GUISize::from_pixels(800, 300));
+    window.set_size(GUISize::from_logical_pixels(800., 300.));
     window.set_title("Hello");
     window.set_background_color(GUIColor::from_rgba_u8u8u8u8(50, 50, 50, 255));
 
